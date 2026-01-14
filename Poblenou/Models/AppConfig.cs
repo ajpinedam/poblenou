588d6557f3e3e2434a577e027b7ee81ecdb4451c
Andres Pineda <pineda.andres@gmail.com>
2026-01-13 23:45:32 -0500

    Initial commit

---
+++ Poblenou/Models/AppConfig.cs
@@ -0,0 +1,6 @@
+namespace Poblenou.Models;
+
+public record AppConfig
+{
+    public string? Environment { get; init; }
+}
---
+++ Poblenou/Presentation/SecondModel.cs
@@ -0,0 +1,5 @@
+namespace Poblenou.Presentation;
+
+public partial record SecondModel(Entity Entity)
+{
+}
